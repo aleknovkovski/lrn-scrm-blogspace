52a88f41ddde30785aecd38ae72ac701e8d6585f
Alek Novkovski <alek@greatcode2.com>
2022-11-28 23:29:00 +0100

    Render first 5 posts with title and body

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 fetch("https://apis.scrimba.com/jsonplaceholder/posts")
 .then(res => res.json())
-.then(js => console.log(js.slice(0,5)))
+.then(js => {
+    const posts = js.slice(0, 5)
+    console.log(posts)
+    let markup = ""
+    posts.map((post)=> {
+        markup += `
+        <h2>${post.title}</h2>
+        <p>${post.body}</p>
+        `
+    })
+    document.getElementById('blog-posts').innerHTML = markup
+})
