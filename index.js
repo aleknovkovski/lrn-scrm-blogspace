d261282e601413b6048b74af90b7282fcb2cfa2d
Alek Novkovski <alek@greatcode2.com>
2022-11-29 01:12:50 +0100

    Remove previous challenge, add new challenge

--- index.js
+++ index.js
@@ -12,3 +12,14 @@ fetch("https://apis.scrimba.com/jsonplaceholder/posts")
     })
     document.getElementById('blog-posts').innerHTML = markup
 })
+
+
+/**
+ Challenge:
+
+ * Listen for the "submit" event on the form (which will happen when the button is clicked)
+ * (Don't forget to preventDefault on the form so it doesn't refresh your page. Google "form preventDefault" if you're not sure what I'm talking about)
+ * Combine the title value and body value into an object (with a "title" property and "body" property)
+ * Log the object to the console
+
+ */
